b8d9bb84e75e9f479123a0aadf93249fea6e6435
Sarthak Thakur <sarthakthakur0097@gmail.com>
2020-03-26 16:37:25 -0400

    react skeleton implemented

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,13 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
 
         }
@@ -43,13 +50,17 @@ namespace API
             {
                 app.UseDeveloperExceptionPage();
             }
-
+             app.UseRouting();
             //app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
 
-            app.UseRouting();
+            app.UseEndpoints(endpoints => 
+            {
+                endpoints.MapControllers();
+            });
 
+           
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
