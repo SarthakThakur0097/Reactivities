bd6c778fe67d4e07cfc19a8fb61b34d7007b4e7a
Sarthak Thakur <sarthakthakur0097@gmail.com>
2020-03-27 16:52:11 -0400

    Cleaned up React file structure and added Hooks

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Header, Icon, List } from 'semantic-ui-react'
+import axios from 'axios';
+import { IActivity } from '../models/activity';
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([])
+  // componentDidMount(){
+  //     axios.get<IActivity[]>('http://localhost:5000/api/activities')
+  //       .then((response) =>{
+  //       this.setState({
+  //         activities: response.data
+  //       })
+  //     })
+  //   }
+
+    return (
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+            {activities.map((activity) => ( 
+              <List.Item key={activity.id}>{activity.title}</List.Item>
+            ))}
+        </List>
+      </div>
+    );
+  }
+
+
+export default App;
