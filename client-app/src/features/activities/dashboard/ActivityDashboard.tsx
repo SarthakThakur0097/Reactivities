3399d78ba6f4ad8616601ddeb3454d01e311d5ed
Sarthak Thakur <sarthakthakur0097@gmail.com>
2020-03-29 20:27:02 -0400

    stuck on an error

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,20 +3,27 @@ import { Grid, List } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
+import { AcitivityForm } from "../form/AcitivityForm";
 
 interface IProps {
-    activities: IActivity []
+  activities: IActivity[];
+  selectActivity: (id: string) => void;
+  selectedActivity: IActivity | null;
 }
-export const ActivityDashboard: React.FC<IProps> = ({activities}) => {
+
+const ActivityDashboard: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  selectedActivity
+}) => {
   return (
     <Grid>
       <Grid.Column width={10}>
-          <ActivityList activities={activities}/>
-
-
+        <ActivityList activities={activities} selectActivity={selectActivity} />
       </Grid.Column>
       <Grid.Column width={6}>
-          <ActivityDetails/>
+        {selectActivity && <ActivityDetails activity={selectedActivity!} />}
+        <AcitivityForm />
       </Grid.Column>
     </Grid>
   );
